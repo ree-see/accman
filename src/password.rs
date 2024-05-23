4095299183709fefe41899e5434880b5f19d6f77
Reesee <j.risse14@gmail.com>
2024-05-23 19:44:02 -0400

    added from trait for Password

--- src/password.rs
+++ src/password.rs
@@ -1,19 +1,20 @@
-// use thiserror;
-
 pub mod password {
+    use thiserror;
 
     #[derive(Debug, Clone, PartialEq, Hash)]
     pub struct Password {
         value: String
     }
-    //
-    // #[derive(thiserror::Error, Debug)]
-    // enum PasswordCreationError {
-    //     TooShort(String),
-    //     NoSpecialChars(String),
-    //     NoCapLetters(String),
-    //     DoesNotMatch(String),
-    // }
+    
+    #[derive(thiserror::Error, Debug)]
+    enum PasswordCreationError {
+        #[error("Password must be 8 characters long")]
+        TooShort(String),
+        #[error("Password must contain [a-z, A-Z, 0-9] only")]
+        NoSpecialChars(String),
+        #[error("Passwords do not match")]
+        DoesNotMatch(String),
+    }
 
     impl Password {
         pub fn new(input: String) -> Self {
@@ -26,6 +27,16 @@ pub mod password {
 
         pub fn set_password(&mut self, new_password: String) {
             self.value = new_password        
+        }
+        
+        pub fn len(&self) -> usize {
+           self.value.len()
+        }
+    }
+
+    impl From<&str> for Password {
+        fn from(value: &str) -> Self {
+           Self { value: value.into() }
         }
     }
 
@@ -47,8 +58,20 @@ pub mod password {
         #[test]
         fn test_set_password() {
             let mut password = Password::new(String::from("hello"));
-            &password.set_password("newhello".into());
+            let _ = &password.set_password("newhello".into());
             assert_eq!(Password::new("newhello".into()), password);
+        }
+
+        #[test]
+        fn test_password_from_string() {
+            let password = Password::from("hello");
+            assert_eq!(Password::new("hello".into()), password.clone());
+        }
+
+        #[test]
+        fn test_password_length() {
+            let password = Password::from("hello");
+            assert_eq!(password.get_password().len(), 5);
         }
     }
 }
